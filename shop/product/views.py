cd40002cbbd2c30da124cbecc8d472457c1f9860
YA-HUICHENG <cocoppa3691@gmail.com>
2019-01-07 01:00:29 +0800

    8

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from product.models import Product
+from product.models import Product, Comment
 
 def product(request):
     '''
     Render the product page
     '''
-    products = Product.objects.all()
+    products = {}
+    for product in Product.objects.all():
+        products.update({product:Comment.objects.filter(product=product)})
     context = {'products':products}
     return render(request, 'product/product.html', context)
 # Create your views here.
