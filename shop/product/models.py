67859f17c8ea4818d1d7949251553b9408576142
YA-HUICHENG <cocoppa3691@gmail.com>
2019-01-13 03:51:47 +0800

    order

--- shop/product/models.py
+++ shop/product/models.py
@@ -5,8 +5,9 @@ class Product(models.Model):
     title = models.CharField(max_length=128, unique=True)
     content = models.TextField()
     pubDateTime = models.DateTimeField(auto_now_add=True)
-    price = models.CharField(max_length=128)
-
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    stock = models.PositiveIntegerField()
+    
     def __str__(self):
         return self.title
 
